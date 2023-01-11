0f7c4f7ab3e1834ae27d27610faab3fd4ec4bfb3
Jie eun Lee <jieeunlee91@gmail.com>
2023-01-10 20:43:42 -0500

    api is ready

---
+++ src/components/Cocktail.js
@@ -0,0 +1,31 @@
+// import RandomCocktail from './RandomCocktail';
+import { useState, useEffect } from 'react';
+
+
+// import axios from 'axios';
+
+const Cocktail = () => {
+
+     useEffect( () => {
+        console.log ('side effect is running 🏃‍♀️')        
+
+        const url = new URL('https://thecocktaildb.com/api/json/v1/1/random.php');
+        
+        url.search = new URLSearchParams({
+            name: 'strDrink',
+            image: 'strDrinkThumb',
+            instruction: 'strInstructions'            
+        })
+        
+        fetch(url)
+            .then( (response) => {
+                return response.json()
+            })
+            .then( (data) => {
+                console.log(data)
+
+            })
+    }, []);
+}
+
+export default Cocktail;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Your Favorite Cocktail App 🍷</h1>
+        </header>
+    )
+}
+
+export default Header;
